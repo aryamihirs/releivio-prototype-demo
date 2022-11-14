551d07ddb7b8a029006dc42b929665a3bb2c730b
Arya Mihir Singh <s.aryamihir@gmail.com>
2022-11-13 22:29:04 -0800

    changes to generate mlModel

--- tpot_pipeline.py
+++ tpot_pipeline.py
@@ -10,32 +10,37 @@ tpot_data = np.recfromcsv('dataset/dataframe_hrv_test.csv', delimiter=',', dtype
 # print(tpot_data['stress'].size())
 
 for i in range(len(tpot_data['stress'])):
-    if(tpot_data['stress'][i] < 0.4):
+    if (tpot_data['stress'][i] < 0.4):
         tpot_data['stress'][i] = 0
-    elif(tpot_data['stress'][i] < 0.6):
+    elif (tpot_data['stress'][i] < 0.6):
         tpot_data['stress'][i] = 1
-    elif(tpot_data['stress'][i] < 0.8):
+    elif (tpot_data['stress'][i] < 0.8):
         tpot_data['stress'][i] = 2
-    elif(tpot_data['stress'][i] <= 1):
+    elif (tpot_data['stress'][i] <= 1):
         tpot_data['stress'][i] = 3
 
 print(tpot_data['stress'][44])
 
-features = np.delete(tpot_data.view(np.float64).reshape(tpot_data.size, -1), tpot_data.dtype.names.index('stress'), axis=1)
+# class labels convert to int
+target = tpot_data['stress'].astype('int')
+
+features = np.delete(tpot_data.view(np.float64).reshape(tpot_data.size, -1), tpot_data.dtype.names.index('stress'),
+                     axis=1)
 
 print(features)
 
 training_features, testing_features, training_target, testing_target = \
-    train_test_split(features, tpot_data['stress'], random_state=42)
+    train_test_split(features, target, random_state=42)
 
 # print(training_target.)
 
-exported_pipeline = KNeighborsClassifier(n_neighbors=4, weights="distance")
+# weights parameter 'distance'-> 'uniform' since 'distance' is not supported for CoreML conversion
+exported_pipeline = KNeighborsClassifier(n_neighbors=4, weights="uniform")
 
 # training the model
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
-
+print(results)
 # input_features just shows as sample
 input_features = ["heartRate", "SDNN"]
 output_features = "stress"
